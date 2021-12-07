1457b32a678116c87878d5f588211609f2be08c2
Enmanuel Molina <enmanueldavidmolina@gmail.com>
2021-12-07 02:06:25 -0400

    add delete route

--- src/main/java/com/example/demo/GeometricController.java
+++ src/main/java/com/example/demo/GeometricController.java
@@ -4,10 +4,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.entities.GeometricFigureEntity;
@@ -36,7 +40,7 @@ public class GeometricController {
 		/*if(body.getType().toLowerCase().equalsIgnoreCase("circle")) {
 			figure=new GeometricFigureFactory().setRadius(body.getRadius()).createFigure(Type.CIRCLE);
 		}*/
-		switch (body.getType()) {
+		switch (body.getType().toLowerCase()) {
 		case "circle":
 			figure = new GeometricFigureFactory().setRadius(body.getRadius()).createFigure(Type.CIRCLE);
 			break;
@@ -47,12 +51,16 @@ public class GeometricController {
 			figure = new GeometricFigureFactory().setWidth(body.getWidth()).createFigure(Type.SQUARE);
 			break;
 		}
-		
 		figuresRepository.save(body);
 		//figures.add(figure);
 		return figure;
 
 		//return ResponseEntity.ok(HttpStatus.OK);
+	}
+	@DeleteMapping("/geometric/delete")
+	public ResponseEntity remove(@RequestParam Long id) {
+		figuresRepository.deleteById(id);
+		return ResponseEntity.ok(HttpStatus.OK);
 	}
 }
 
